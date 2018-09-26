7ace8c356333396ec52799a254bb1964ec6dfd67
Sung Bae <singsang2@gmail.com>
2018-09-26 03:36:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var katzDeliLine = [];
 
 function takeANumber(current, person){
   current.push(person)
-  return(`Welcome, ${person}. You are number ${current.length()} in line.`)
+  return(`Welcome, ${person}. You are number ${current.length} in line.`)
 }
 
 function nowServing(current){
-  if (current.length() === 0) {
+  if (current.length === 0) {
     return "There is nobody waiting to be served!"
   }
   else{
@@ -16,13 +16,13 @@ function nowServing(current){
 }
 
 function currentLine(current){
-  if (current.length() === 0){
+  if (current.length === 0){
     return "The line is currently empty."
   }
   else {
     var s = "The line is currently: "
-    for(let i = 0; i < current.length(); i++){
-      if(i < current.length()-1){
+    for(let i = 0; i < current.length; i++){
+      if(i < current.length-1){
         s = s + `${i+1}. ${current[i]}, `
       }
       else{
