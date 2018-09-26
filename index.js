bd656e9a79cd7ade68faa1e3b54f55eeeab7870d
Sung Bae <singsang2@gmail.com>
2018-09-26 03:34:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,20 @@ function nowServing(current){
     return p
   }
 }
+
+function currentLine(current){
+  if (current.length() === 0){
+    return "The line is currently empty."
+  }
+  else {
+    var s = "The line is currently: "
+    for(let i = 0; i < current.length; i++){
+      if(i < current.length-1){
+        s = s + `${i+1}. ${current[i]}, `
+      }
+      else{
+        s = s + `${i+1}. ${current[i]}`
+      }
+    }
+  }
+}
